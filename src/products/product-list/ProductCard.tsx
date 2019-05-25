7e8f659dd4e35ec5b298fbde5dd91ad18c9febd8
Jon Onstott <jon@eaglecapsoftware.com>
2019-05-25 14:47:56 -0700

    Improve styling

--- src/products/product-list/ProductCard.tsx
+++ src/products/product-list/ProductCard.tsx
@@ -7,66 +7,51 @@ type ProductCardProps = {
 	product: Product
 }
 
-type ProductCardState = {
-	isHovered: boolean
-}
-
 export default class ProductCard extends Component<ProductCardProps> {
-
-	state: ProductCardState = {
-		isHovered: false
-	};
 
 	constructor(props: Readonly<ProductCardProps>) {
 		super(props);
 
-		this.onMouseEnter = this.onMouseEnter.bind(this);
-		this.onMouseLeave = this.onMouseLeave.bind(this);
 		this.getActions = this.getActions.bind(this);
 	}
 
-	private onMouseEnter(): void {
-		this.setState({isHovered: true});
-	}
-
-	private onMouseLeave(): void {
-		this.setState({isHovered: false});
-	}
-
 	private getActions(): ReactElement[] {
-		const product = this.props.product;
-		if (this.state.isHovered) {
-			return [<div className="view-details-title">View Details</div>];
-		} else {
-			const hasRating = typeof product.reviewRating === 'number';
-			if (!hasRating) {
-				return [];
-			}
-			return [<Rate defaultValue={product.reviewRating} disabled/>];
+		if (typeof this.props.product.reviewRating === 'number') {
+			return [this.createReviewsElement()];
 		}
+		return [];
+	}
+
+	private createReviewsElement(): ReactElement {
+		const product = this.props.product;
+		return (
+			<div className="product-reviews">
+				<Rate value={product.reviewRating} allowHalf={true} disabled/>
+				<div>{product.reviewCount} reviews</div>
+			</div>
+		);
 	}
 
 	render(): ReactElement {
 		const product = this.props.product;
 		const imageUrl = product.getImageUrl();
-
 		return (
-			<Card className="product-list-card"
-				  hoverable
-				  onMouseEnter={this.onMouseEnter}
-				  onMouseLeave={this.onMouseLeave}
-				  actions={this.getActions()}>
-				<div className="product-name">{product.productName}</div>
+			<Card actions={this.getActions()}>
+				<div className="product-card-content">
+					<div className="product-name">{product.productName}</div>
 
-				{imageUrl && <img className="product-image" alt="Product" src={imageUrl}/>}
+					{/* Typically we'd need thumbnail versions of the images */}
+					{imageUrl && <div className="product-image"><img alt="Product" src={imageUrl}/></div>}
 
-				<Statistic className="product-price" value={product.price}/>
+					<Statistic className="product-price" value={product.price}/>
 
-				{/*	dangerouslySetInnerHTML is used in order to display the description's HTML... we're
-				trusting the DB for having safe data to display, but typically we wouldn't do this */}
-				<div className="product-summary" dangerouslySetInnerHTML={{__html: product.shortDescription || '<i>View details</i>'}}/>
+					{/*	dangerouslySetInnerHTML is used in order to display the description's HTML... we're
+						trusting the DB for having safe data to display, but typically we wouldn't do this */}
+					<div className="product-summary" dangerouslySetInnerHTML={{__html: product.shortDescription || '<i>View details</i>'}}/>
 
-				<div className="card-overlay bottom-white-mask"/>
+					{/* Fade-out text at the bottom */}
+					<div className="card-overlay bottom-white-mask"/>
+				</div>
 			</Card>
 		);
 	}
